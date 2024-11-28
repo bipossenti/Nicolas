1fbc9d1a00e6548c0ab731d8f0b2e7cafd41ccb2
bianca possenti <107114103+bipossenti@users.noreply.github.com>
2024-11-27 21:56:40 -0300

    Create informacoesGlobais.js

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,9 @@
+import { incluirTexto } from "./common.js";
+
+const texto = `
+O uso de redes sociais é um fenômeno global, conectando bilhões de pessoas ao redor do mundo. Entre as mais utilizadas estão 
+<span>Facebook</span>, <span>Instagram</span> e <span>WhatsApp</span>, que têm dominado os rankings em popularidade e 
+engajamento. Esta análise explora a influência e o impacto das redes sociais em escala global.
+`;
+
+incluirTexto(texto);
